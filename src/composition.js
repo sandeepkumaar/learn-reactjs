d61e68ff2c2d71da8ce305fb144ba80fa2445144
sandeepkumaar.c <cskumaar1992@gmail.com>
2019-02-05 19:05:30 +0530

    composition: pass components thru props

--- src/composition.js
+++ src/composition.js
@@ -3,19 +3,29 @@ const React = require('react');
 const FancyBorder = function FancyBorder(props) {
 
   console.log('FancyBorder', props.children); // child element
-  let style = { border: "1px solid green"};
+  let greenStyle = { border: "1px solid green"};
+  let blueStyle = { border: "1px solid blue"};
   return (
-    <div style= { style }>
-
+    <div>
+      <div style={ greenStyle }> { props.children } </div> <br/>
+      <div style={ blueStyle }> { props.attr } </div>
     </div>
   )
 };
 
 
+
 const Box = function Box(props) {
   console.log('Box', props);
+  const AttribComp = function AttribComp() {
+    return (
+      <p> component passed as the attribute/props </p>
+    )
+  }
+
+
   return (
-    <FancyBorder>
+    <FancyBorder attr={ <AttribComp/> }>
       child element
     </FancyBorder>
   )
